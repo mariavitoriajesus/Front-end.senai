d83e8c4d74708fd6d6993fb14c80a4391f67fabd
maliknkosi <mariavitoria.jesus2003@gmail.com>
2024-11-14 16:28:19 -0300

    Aula 1 - Introdução a JavaScript

---
+++ CONTEUDO/JavaScript/aula1-Introducao/script.js
@@ -0,0 +1,18 @@
+// string - caractere
+// number - numero(inteiro ou real)
+// Boolean - verdadeiro ou FontFaceSetLoadEvent, ou seja lógico 
+// Array - vetor, ou lista
+
+// alert('é aqui onde começa o javascript') 
+// prompt('Vamos entender melhor?')
+// confirm('Deseja aprender realmente?')
+
+// var num = 11
+// var ligado = false
+// var ligado = true
+// var valorTotal = 1.7
+// var lista =[]
+
+var nome = prompt('Qual é o seu nome?')
+var aniversario = prompt('Qual é a data do seu aniversário? ')
+alert("Olá " + nome + "o dia dp seu aniversário é " + aniversario)
---
+++ CONTEUDO/JavaScript/aula1-Introducao/somar.js
@@ -0,0 +1,12 @@
+var n1 = prompt('Digite o primeiro número: ')
+var n2 = prompt('Digite o segundo número: ')
+
+var soma = Number(n1) + Number(n2) 
+alert('A soma dos números é de: ' + soma)
+
+// var - global e sobrescrevivel 
+var variavel_global = 1
+// let - local e sobrescrevivel
+let variavel_local = 2
+//cont - global e sobrescrevivel
+const variavel_constante = "fixo"
